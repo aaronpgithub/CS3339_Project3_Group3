4b5e2a0106e137fb78c3180d2aba6c73cd19ffeb
donnybuns1 <111816421+donnybuns1@users.noreply.github.com>
2022-11-26 19:28:18 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ARM-v8-disassembler
+In this project you will create a simplified ARM v8 disassembler. Your disassembler will read in a binary file containing an ARM program in machine code and output the assembly code that generated the machine code.
+# CS3339_Project3_Group3
---
+++ fetch.go
@@ -0,0 +1,19 @@
+package main
+
+func fetch() {
+	// can fetch and decode 2 instructions per clock cycle
+	// check if fetch is stalled bc branch/cache miss
+	// check if instruction in cache
+	// if instruction not in cache then grab from memory and it will be the instruction in cache
+	// for the next cycle
+	// instructions that are branched over need to be revisited if needed
+	// writing takes the first part then reading the second
+	// when BREAK is fetched no more instructions will be fetched
+	// Branch, BREAK, and NOP instructions will all be fetched,
+	// but will not be written into the Pre-Issue Buffer
+
+	/*if typeofInstruction == "B" || cacheHit == true {
+		fetch is stalled
+	} */
+
+}
